de936fce4fabfac0dade5a2af5ab46639abf7ef1
josericardopenase <josericardopenase@gmail.com>
2023-05-09 12:02:16 +0100

    Auth: added auth with firebase, register and login

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,7 +1,13 @@
 import { Injectable } from '@angular/core';
 import { collection, doc, onSnapshot } from 'firebase/firestore';
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from 'firebase/auth';
 import { db } from '../../../firebase';
 import { UserLogin, UserRegister } from 'src/types/db/user';
+import { create } from 'domain';
 
 @Injectable({
   providedIn: 'root',
@@ -13,7 +19,36 @@ export class AuthService {
     return false;
   }
 
-  public login(payload: UserLogin) {}
-  public register(payload: UserRegister) {}
+  public async login(payload: UserLogin) {
+    const auth = getAuth();
+    try {
+      const response = await signInWithEmailAndPassword(
+        auth,
+        payload.email,
+        payload.password
+      );
+      console.log(response.user.email);
+      console.log('login success');
+    } catch (err: any) {
+      const errorCode = err.code;
+      const errorMessage = err.message;
+      console.log('error login');
+    }
+  }
+  public async register(payload: UserRegister) {
+    const auth = getAuth();
+
+    try {
+      const response = await createUserWithEmailAndPassword(
+        auth,
+        payload.email,
+        payload.password
+      );
+      console.log(response.user.email);
+      console.log('success');
+    } catch (err: any) {
+      console.log('error');
+    }
+  }
   public logOut() {}
 }
