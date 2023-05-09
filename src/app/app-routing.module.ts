884279757c826b61de5724d08777b81556157ef2
josericardopenase <josericardopenase@gmail.com>
2023-05-09 11:08:23 +0100

    Added tab navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,37 +7,6 @@ const routes: Routes = [
     loadChildren: () =>
       import('./pages/app/tabs/tabs.module').then((m) => m.TabsPageModule),
   },
-  {
-    path: 'news',
-    loadChildren: () =>
-      import('./pages/app/news/news.module').then((m) => m.NewsPageModule),
-  },
-  {
-    path: 'profile',
-    loadChildren: () =>
-      import('./pages/app/profile/profile.module').then(
-        (m) => m.ProfilePageModule
-      ),
-  },
-  {
-    path: 'favourites',
-    loadChildren: () =>
-      import('./pages/app/favourites/favourites.module').then(
-        (m) => m.FavouritesPageModule
-      ),
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./pages/auth/login/login.module').then( m => m.LoginPageModule)
-  },
-  {
-    path: 'register',
-    loadChildren: () => import('./pages/auth/register/register.module').then( m => m.RegisterPageModule)
-  },
-  {
-    path: 'news-detailed',
-    loadChildren: () => import('./pages/app/news-detailed/news-detailed.module').then( m => m.NewsDetailedPageModule)
-  },
 ];
 @NgModule({
   imports: [
