cde3dc58ed2387ec15e5275c1c9cdf84713cf89e
Joaquín Villamonte Pereira <125866766+jan-qoui@users.noreply.github.com>
2023-05-24 13:51:22 +0100

    profile typescript stub

--- src/app/pages/app/profile/profile.page.ts
+++ src/app/pages/app/profile/profile.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { DocumentData } from 'firebase/firestore';
 import { ProfileService } from 'src/app/services/app/profile/profile.service';
 import { AuthService } from 'src/app/services/auth/auth.service';
@@ -11,11 +12,25 @@ import { User } from 'src/types/db/user';
 })
 export class ProfilePage implements OnInit {
   profile!: DocumentData;
+  modifying = false;
+
   constructor(public auth: AuthService, public profService: ProfileService) {}
 
   ngOnInit() {
     this.profService.getProfile(this.auth.user?.uid ?? "").then(x => {
       this.profile = x.docs[0].data();
     })
+  }
+
+  clickEdit(update: any) {
+    if (this.modifying) {
+      this.submitChanges(update)
+    }
+    
+    this.modifying = !this.modifying;
+  }
+
+  submitChanges(update: any) {
+    this.profService.updateProfile(this.auth.user?.uid ?? '', update)
   }
 }
