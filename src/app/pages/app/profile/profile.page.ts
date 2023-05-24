a308b8a6958d728e695a2ce4ed5ac224602bd477
josericardopenase <josericardopenase@gmail.com>
2023-05-24 14:01:41 +0100

    Profile : added inputs to modify profile

--- src/app/pages/app/profile/profile.page.ts
+++ src/app/pages/app/profile/profile.page.ts
@@ -11,11 +11,22 @@ import { User } from 'src/types/db/user';
 })
 export class ProfilePage implements OnInit {
   profile!: DocumentData;
+  editEmail = false;
+  editLastName = false;
+  editFirstName = false;
+  editUserName = false;
   constructor(public auth: AuthService, public profService: ProfileService) {}
 
+  disableAllEdits() {
+    this.editEmail = false;
+    this.editLastName = false;
+    this.editFirstName = false;
+    this.editUserName = false;
+  }
+
   ngOnInit() {
-    this.profService.getProfile(this.auth.user?.uid ?? "").then(x => {
+    this.profService.getProfile(this.auth.user?.uid ?? '').then((x) => {
       this.profile = x.docs[0].data();
-    })
+    });
   }
 }
